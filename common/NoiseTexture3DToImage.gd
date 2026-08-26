a1a19b4e43edb3c5af6301509d52ae8f32776327
lonevox <quinn.epicness@gmail.com>
2026-08-27 10:52:47 +1200

    Add editor button to generate 3D noise instead of on _ready

--- common/NoiseTexture3DToImage.gd
+++ common/NoiseTexture3DToImage.gd
@@ -1,8 +1,10 @@
+@tool
 extends Node
 
+@export_tool_button("Save to File") var save_to_file := _on_save_to_file_button_pressed
 @export var noise_texture_3d: NoiseTexture3D
 
-func _ready() -> void:
+func _on_save_to_file_button_pressed():
 	var image_texture := ImageTexture3D.new()
 	image_texture.create(noise_texture_3d.get_format(), noise_texture_3d.width, noise_texture_3d.height, noise_texture_3d.depth, noise_texture_3d.has_mipmaps(), noise_texture_3d.get_data())
 	ResourceSaver.save(image_texture, "res://noise_texture_out.tres")
